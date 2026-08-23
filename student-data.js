28360e8d5c1497b4237dbca2080772537bf1806d
Bible College <157099671+biblecollege2026@users.noreply.github.com>
2026-08-23 23:25:01 +0530

    Add files via upload

---
+++ student-data.js
@@ -0,0 +1,123 @@
+// student-data.js
+// Bible College of India - Student Data Storage
+
+const STUDENT_DATA = {
+    // Academic Performance Data
+  // Academic Performance Data
+    marks: {
+        'hiwalefrancis@gmail.com': { name: 'Francis hiwali', marks: [95, 100, 95, null, 100, 100, 93], offlineMark: 77.50 },
+        'jadhavsushma05@gmail.com': { name: 'Sushma Jadhav', marks: [55, 40, 62, null, 55, 57, 55], offlineMark: 42 },
+        'shwetadalge14@gmail.com': { name: 'Shweta Shirish dalge', marks: [94, 53, 92, 70, 75, 57, 77], offlineMark: 76 },
+        'jlibiblecollege@gmail.com': { name: 'Bible College Admin', marks: [], offlineMark: 0 },
+        
+    },
+
+    // Student Profile Information
+    profiles: {
+        'hiwalefrancis@gmail.com': {
+        name: 'HIWALE FRANCIS TATYABA',
+        address: 'A/P. Haregaon. D Quarter 10A Tal--Shrirampur Dist --Ahilyanagar pin code --413718',
+        mobile: '9763532008',
+        mobile2: '9763532008',
+        dob: '6/1/1972',
+        age: 53,
+        gender: 'Male',
+        education: 'Masters',
+        occupation: 'Service',
+        marital: 'Married',
+        church: 'St Teresa Church Haregaon Tal--Shrirampur',
+        pastor: 'Pastor Name Not Specified',
+        reference1: 'Kalpana Hiwale - 9021887272',
+        reference2: 'Yuvraj Hiwale - 8767850292',
+        futurePlan: 'Declaration for kingdom of God second coming of Jesus. To give good news for every'
+        },
+        'varshabhore55@gmail.com': {
+        name: 'Varsha ram bhore',
+        address: '411046',
+        mobile: '8975501278',
+        mobile2: '9923100225',
+        dob: '6/7/1978',
+        age: 42,
+        gender: 'Female',
+        education: '12th',
+        occupation: 'None',
+        marital: 'Married',
+        church: 'Pavitra naam devaly panchhaud',
+        pastor: 'Pastor Name Not Specified',
+        reference1: '9130841225',
+        reference2: 'Swati',
+        futurePlan: 'Dev margdarshan karel tss'
+        },
+        'jadhavsushma05@gmail.com': {
+        name: 'Sushma Jadhav',
+        address: 'Pt no-18, Veal Colony, TC College road, Baramati-413102 Pune',
+        mobile: '8999256704',
+        mobile2: '7387900337',
+        dob: '6/1/1970',
+        age: 55,
+        gender: 'Female',
+        education: '12th',
+        occupation: 'None',
+        marital: 'Married',
+        church: 'Church of Christ, Baramati',
+        pastor: 'Pastor Anosh Khawadia',
+        reference1: 'Priyanka - 9325441937',
+        reference2: 'Andrew - 9011038692',
+        futurePlan: 'Not Specified'
+        },
+        'shwetadalge14@gmail.com': {
+        name: 'Shweta Shirish dalge',
+        address: '26, Ashirwad Colony s a mission taloda Road Dis. Nandurbar',
+        mobile: '8237448272',
+        mobile2: '8237448272',
+        dob: '8/14/2025',
+        age: 40,
+        gender: 'Female',
+        education: 'Graduate',
+        occupation: 'Service',
+        marital: 'Married',
+        church: 'Suvartha Alliance ministry Nandurbar',
+        pastor: 'Pastor Anup valvi',
+        reference1: 'Manda valvi',
+        reference2: 'Vaishali chakrnaryan',
+        futurePlan: 'स्वतः देवावर विश्वास ठेवून इतरांना देवाच्या नावाची ओळख पटवून देणे. देवाच्या वचनाची ओळख करून देणे'
+        },
+         'sagarware12@gmail.com': {
+        name: 'Sagar ware',
+        address: 'Vittalnagar aambedkar bhavana aankalkhop 416316',
+        mobile: '7558630616',
+        mobile2: '7558630616',
+        dob: '6/13/1987',
+        age: 37,
+        gender: 'Male',
+        education: '10th',
+        occupation: 'None',
+        marital: 'Married',
+        church: 'Bethel fellowship ashta',
+        pastor: 'Pastor Name Not Specified',
+        reference1: 'Atish bro',
+        reference2: 'manoj bro',
+        futurePlan: 'Gospel preaching'
+        },
+       
+        'jlibiblecollege@gmail.com': {
+            name: 'Bible College Admin',
+            address: 'Bible College of India',
+            mobile: 'N/A',
+            dob: 'N/A',
+            age: 'N/A',
+            gender: 'N/A',
+            education: 'N/A',
+            occupation: 'Administrator',
+            marital: 'N/A',
+            church: 'Jesus Loves India Church Foundation',
+            pastor: 'N/A',
+            reference1: 'N/A',
+            reference2: 'N/A',
+            futurePlan: 'Administering Bible College'
+        }
+    },
+
+    // Exam months mapping (updated to include offline)
+    examMonths: ['SEPTEMBER EXAM 2025', 'OCTOBER EXAM 2025', 'NOVEMBER EXAM 2025', 'DECEMBER EXAM 2025', 'JANUARY EXAM 2026', 'FEBRUARY EXAM 1 ', 'FEBRUARY EXAM 2', 'OFFLINE EXAM MARCH 2026']
+};
